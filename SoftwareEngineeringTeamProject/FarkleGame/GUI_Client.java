15fa0bf280132a5d51f91e4af07f66f4b351de17
Shandon M. Probst <sprobst@cub.uca.edu>
2023-04-17 23:54:09 -0500

    Renamed ClientGUI to GUI_Client
    Added specification to the name.

--- SoftwareEngineeringTeamProject/FarkleGame/GUI_Client.java
+++ SoftwareEngineeringTeamProject/FarkleGame/GUI_Client.java
@@ -1,14 +1,14 @@
-package client;
+package FarkleGame;
 
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
-public class ClientGUI {    // Your class name
+public class GUI_Client {    // Your class name
 
    public static void main(String[] args) {
       
-      JFrame f = new JFrame("A JFrame");
+      JFrame f = new JFrame("Client");
       f.setSize(250, 250);
       f.setLocation(300,200);
       f.getContentPane().add(BorderLayout.CENTER, new JTextArea(10, 40));
